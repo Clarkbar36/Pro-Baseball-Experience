97b3190bea4c4e5cf2ef39c52b0b7419eb5f55d2
Alex Clark <alexclark36@gmail.com>
2019-05-10 11:17:21 -0400

    adjusted scatter options

--- Codes/Team_Scatter.R
+++ Codes/Team_Scatter.R
@@ -122,7 +122,7 @@ all.stats <- merge(tm_bat,tm_pitch)
 all.stats <- merge(all.stats,tm_field)
 all.stats <- merge(all.stats,lkup,all.x = TRUE)
 
-cnames <- colnames(all.stats[c(3:69,72:74)])
+cnames <- colnames(all.stats[c(3:69,73:74)])
 cnames <- sort(cnames)
 
 gms <- read.csv("Exports/games.csv")
